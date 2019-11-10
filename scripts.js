49aa0af4768bf96d26eb365deae3482a4d4f6f0e
Евгений Цыганков <valeriysemenovich1958@gmail.com>
2019-11-10 20:02:32 +0300

    desktop is done

--- scripts.js
+++ scripts.js
@@ -1,31 +1,164 @@
-const icon = document.querySelector(".nav-bar-icon");
+const icon = document.querySelector(".fa-bars");
 const navBar = document.querySelector(".nav-bar");
 const ul = document.querySelector(".nav-bar ul");
 
+const NAV_BAR_HIDE = "nav-bar-hide";
+
 icon.addEventListener("click", () => {
-    
-    if (navBar.style.width == "0px") {
 
-        navBar.style.width = "739px";
-        ul.style.display = "flex";
-        ul.style.opacity = "0";
-        window.setTimeout(() => {
+    if (window.innerWidth < 992) {
 
-            ul.style.opacity = "1";
-            
-        }, 1500);
-    } 
+        if (navBar.classList.contains(NAV_BAR_HIDE)) {
+
+            navBar.classList.remove(NAV_BAR_HIDE);
+            ul.style.opacity = 1;
+            ul.style.display = "flex";
+        }
+
+        if (getComputedStyle(navBar).top == `-${window.innerHeight}px`) {
+
+            navBar.style.top = "5px";
+        }
+        else {
+
+            navBar.style.top = `-${window.innerHeight}px`;
+        }
+    }
     else {
-        
-        navBar.style.width = "0px";
-        ul.style.opacity = "0";
+
+        if (!navBar.classList.contains(NAV_BAR_HIDE)) {
+
+            navBar.classList.add(NAV_BAR_HIDE);
+            ul.style.opacity = 0;
+        }
+        else {
+            ul.style.display = "flex";
+            ul.style.opacity = 0;
+            navBar.classList.remove(NAV_BAR_HIDE);
+    
+            window.setTimeout(() => {
+    
+                ul.style.opacity = 1;
+            }, 1500);
+        }
     }
 });
 
 ul.addEventListener("transitionend", () => {
+        
+    if (window.innerWidth < 1110) {
 
-    if (ul.style.opacity == "0") {
+        
+    }
+    else {
 
-        ul.style.display = "none";
+        if (!navBar.classList.contains(NAV_BAR_HIDE)) {
+
+            ul.style.display = "flex";
+        }
+        else {
+            ul.style.display = "none";
+        }
     }
 });
+
+
+
+/* работающий код скрытия меню */
+
+// icon.addEventListener("click", () => {
+
+//     if (navBar.style.width == "0px") {
+
+//         navBar.style.width = "739px";
+
+//         ul.style.display = "flex";
+//         ul.style.opacity = "0";
+
+//         window.setTimeout(() => {
+
+//             ul.style.opacity = "1";
+
+//         }, 1500);
+//     } 
+//     else {
+
+//         navBar.style.width = "0px";
+//         ul.style.opacity = "0";
+//     }
+// });
+
+// ul.addEventListener("transitionend", () => {
+
+//     if (getComputedStyle(ul).opacity == "0") {
+
+//         ul.style.display = "none";
+//     }
+// });
+
+/* работающий код скрытия меню */
+
+
+
+
+// var map;
+
+// const marksArr = [
+//     [52.387, 31.023],
+//     [52.4195, 31.0047],
+//     [52.4482, 30.968],
+// ]
+
+// const marksCaptions = [
+//     'Белица-Сити',
+//     'Уютный зал в доме',
+//     'ДК Гомсельмаш'
+// ]
+
+// window.onload = () => {
+
+//     map = new ymaps.Map("map", {
+
+//         center: marksArr[0],
+//         zoom: 16
+//     });
+
+//     const marks = [];
+
+//     for (let i = 0; i < 3; i++) {
+
+//         marks[i] = new ymaps.Placemark(marksArr[i], { iconCaption: marksCaptions[i] }, { iconColor: 'red' });
+//     }
+
+//     marks.forEach((item) => {
+
+//         map.geoObjects.add(item);
+//     });
+
+//     const itemsMapLegend = document.querySelectorAll(".map-legend ul li");
+
+//     itemsMapLegend.forEach((item, index) => {
+
+//         item.addEventListener("click", () => {
+
+//             itemsMapLegend.forEach((item) => {
+
+//                 item.classList.remove("active");
+//             });
+
+//             item.className = "active";
+
+//             map.setCenter(marksArr[index]);
+//         });
+//     });
+// }
+
+
+
+// const benefits = document.querySelector(".benefits-content");
+
+// window.addEventListener("scroll", () => {
+
+//     if (pageYOffset == 200) {
+//     }
+// });
